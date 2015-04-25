e8c789627a6b7f666cf444b17d6681fa1a48389a
HansJon <HJIvars@hotmail.com>
2015-04-25 00:34:44 +0000

    Database viewing for client established and completed

--- SoyProject/SoyProject/DatabaseTableForm.Designer.cs
+++ SoyProject/SoyProject/DatabaseTableForm.Designer.cs
@@ -28,25 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("1");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("2");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("3");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("4");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("5");
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("99999");
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem10 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem11 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem12 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem13 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem14 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem15 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem16 = new System.Windows.Forms.ListViewItem("");
             this.LV_DBTable = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // LV_DBTable
             // 
             this.LV_DBTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LV_DBTable.Font = new System.Drawing.Font("MingLiU", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LV_DBTable.FullRowSelect = true;
+            this.LV_DBTable.GridLines = true;
+            listViewItem1.IndentCount = 2;
+            this.LV_DBTable.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5,
+            listViewItem6,
+            listViewItem7,
+            listViewItem8,
+            listViewItem9,
+            listViewItem10,
+            listViewItem11,
+            listViewItem12,
+            listViewItem13,
+            listViewItem14,
+            listViewItem15,
+            listViewItem16});
             this.LV_DBTable.Location = new System.Drawing.Point(0, 0);
             this.LV_DBTable.Name = "LV_DBTable";
-            this.LV_DBTable.Size = new System.Drawing.Size(816, 473);
+            this.LV_DBTable.Size = new System.Drawing.Size(891, 499);
             this.LV_DBTable.TabIndex = 0;
             this.LV_DBTable.UseCompatibleStateImageBehavior = false;
+            this.LV_DBTable.View = System.Windows.Forms.View.Details;
             // 
             // DatabaseTableForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(816, 473);
+            this.ClientSize = new System.Drawing.Size(891, 499);
             this.Controls.Add(this.LV_DBTable);
             this.Name = "DatabaseTableForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DatabaseTableForm";
             this.Load += new System.EventHandler(this.DatabaseTableForm_Load);
             this.ResumeLayout(false);
@@ -56,5 +95,7 @@
         #endregion
 
         private System.Windows.Forms.ListView LV_DBTable;
+
+
     }
 }
--- SoyProject/SoyProject/DatabaseTableForm.cs
+++ SoyProject/SoyProject/DatabaseTableForm.cs
@@ -12,9 +12,13 @@ namespace SoyProject
 {
     public partial class DatabaseTableForm : Form
     {
+        Gagnagrunnur DB = new Gagnagrunnur();
+
         public DatabaseTableForm()
         {
             InitializeComponent();
+
+            DB.ConnectionToDatabase();
         }
 
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
@@ -24,10 +28,61 @@ namespace SoyProject
             return res;
         }
 
+
+        private void ReadDB()
+        {
+            LV_DBTable.Items.Clear();
+
+            // Listinn sem lesinn er úr gagnagrunninum
+            List<string> linur = new List<string>();
+
+            // Fylki notað til að bæta við ListView
+            string[] arr = new string[4];
+
+            // Heldur utan um hlutina sem eru bætt við í hverja línu á ListView
+            ListViewItem itm;
+
+            try
+            {
+                linur = DB.ReadFromDatabase();
+
+                foreach (string lina in linur)
+                {
+                    // Splitta línum frá gagnagrunninum
+                    string[] lineFromDatabase = lina.Split(';');
+
+                    string ID = lineFromDatabase[0];
+                    string name = lineFromDatabase[1];
+                    string bottles = lineFromDatabase[2];
+                    string date = lineFromDatabase[3];
+
+                    // Hefði líka verið hægt að gera þetta svona (arr[9] = lineFromDatabase[0])
+                    arr[0] = ID;
+                    arr[1] = name;
+                    arr[2] = bottles;
+                    arr[3] = date;
+
+                    // Set inn í ListView hlutinn og bæti svo inn í ListView hólfin
+                    itm = new ListViewItem(arr);
+                    LV_DBTable.Items.Add(itm);
+
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Villa: " + exc);
+            }
+        }
+        
         private void DatabaseTableForm_Load(object sender, EventArgs e)
         {
-            LV_DBTable.Items.Add("ID", 200);
-            LV_DBTable.Items.Add("employeeSSN", 200);
+            LV_DBTable.Columns.Add("#", 90);
+            LV_DBTable.Columns.Add("Nafn", 400);
+            LV_DBTable.Columns.Add("Flöskur", 110);
+            LV_DBTable.Columns.Add("Dagsetning", 270);
+
+            ReadDB();
         }
+
     }
 }
